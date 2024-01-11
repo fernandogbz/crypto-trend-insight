2d0d2bcd0d69c80bf59dc5aa5c7b60a408425953
fernandogbz <fgjv06@gmail.com>
2024-01-11 18:46:00 -0300

    Container div styled component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
 
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+`
+
 const Heading = styled.h1`
   font-family:'Lato', sans-serif;
   color: #fff;
@@ -9,10 +15,10 @@ const Heading = styled.h1`
 function App() {
 
   return (
-    <>
+    <Container>
       <Heading>Desde App</Heading>
       
-    </>
+    </Container>
   )
 }
 
