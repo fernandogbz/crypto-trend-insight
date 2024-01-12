b9682d10981ea2bc17f5fae2c653444626e462a0
fernandogbz <fgjv06@gmail.com>
2024-01-12 17:45:54 -0300

    InputSubmit styled component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
+const InputSubmit = styled.input`
+    
+`
+
 const Form = () => {
   return (
     <form>
-        <input type="submit" value="Check" />
+        <InputSubmit 
+            type="submit"
+            value="Check"
+        />
     </form>
   )
 }
