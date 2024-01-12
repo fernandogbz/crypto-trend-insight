8e6b5147e0c4403f1cdad95795a065ab8ca111e5
fernandogbz <fgjv06@gmail.com>
2024-01-12 17:50:21 -0300

    Input submit hover styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,6 +11,12 @@ const InputSubmit = styled.input`
     text-transform: uppercase;
     font-size: 20px;
     border-radius: 5px;
+    transition: background-color .3s ease;
+
+    &:hover {
+        background-color: #7a7dfe;
+        cursor: pointer;
+    }
 `
 
 const Form = () => {
