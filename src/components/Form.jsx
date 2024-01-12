ef74739eb2dea2cb2a15e06e428342fe535523fb
fernandogbz <fgjv06@gmail.com>
2024-01-12 17:46:57 -0300

    InputSubmit css styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import styled from '@emotion/styled'
 
 const InputSubmit = styled.input`
-    
+    background-color: #9497ff;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #fff;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
 `
 
 const Form = () => {
