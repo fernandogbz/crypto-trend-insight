1c0c912a8c36649fd1c0fc532dd0309cc8ddc689
fernandogbz <fgjv06@gmail.com>
2024-01-13 18:58:36 -0300

    Label styles, display block, font and marign

--- src/hooks/useSelectCurrency.jsx
+++ src/hooks/useSelectCurrency.jsx
@@ -3,6 +3,11 @@ import styled from '@emotion/styled'
 
 const Label = styled.label`
     color: #fff;
+    display: block;
+    font-family:'Lato', sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
 `
 
 const useSelectCurrency = (label, options) => {
