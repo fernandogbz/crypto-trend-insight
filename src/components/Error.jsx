0205bc936d3e6f4198b7a8a5c9878d94809f58e7
fernandogbz <fgjv06@gmail.com>
2024-01-16 18:07:04 -0300

    Text styled component for div, styles and text error added as a component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,19 @@
 import styled from '@emotion/styled'
 
+const Text = styled.div`
+    background-color: #b7322c;
+    color: #fff;
+    padding: 15px;
+    font-size: 16px;
+    text-transform: uppercase;
+    font-family: 'Lato', sans-serif;
+    font-weight: 700;
+    text-align: center;
+`
+
 const Error = ({children}) => {
   return (
-    <div>{children}</div>
+    <Text>{children}</Text>
   )
 }
 
