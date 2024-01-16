1c5802d0aaea1d0b5cdd0b82115038bbdd953062
fernandogbz <fgjv06@gmail.com>
2024-01-16 18:16:21 -0300

    Text styled component border left and border-radius added, and color changed slightly

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,11 @@
 import styled from '@emotion/styled'
 
 const Text = styled.div`
-    background-color: #b7322c;
+    background-color: #b7332cab;
     color: #fff;
     padding: 15px;
+    border-left: 5px solid #b91c1c;
+    border-radius: 0 0.5rem 0.5rem 0;
     font-size: 16px;
     text-transform: uppercase;
     font-family: 'Lato', sans-serif;
