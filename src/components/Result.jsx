a313651505b4e79d4e7d37d3bc0458e2981c9ebd
fernandogbz <fgjv06@gmail.com>
2024-01-17 17:40:41 -0300

    Container display flex, and new div for text

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,6 +3,8 @@ import styled from '@emotion/styled'
 const Container = styled.div`
     color: #fff;
     font-family: 'Lato', sans-serif;
+
+    display: flex;
 `
 
 const Text = styled.p`
@@ -23,11 +25,14 @@ const Result = ({result}) => {
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = result
   return (
     <Container>
-        <Price>Price is: <span>{PRICE}</span></Price>
-        <Text>Highest price of the day: <span></span>{HIGHDAY}</Text>
-        <Text>Lowest price of the day: <span></span>{LOWDAY}</Text>
-        <Text>Variation last 24 hours: <span></span>{CHANGEPCT24HOUR}</Text>
-        <Text>Last Update: <span></span>{LASTUPDATE}</Text>
+        <img src={`https://cryptocompare.com/${IMAGEURL}`} alt="crypto image" />
+        <div>
+            <Price>Price is: <span>{PRICE}</span></Price>
+            <Text>Highest price of the day: <span></span>{HIGHDAY}</Text>
+            <Text>Lowest price of the day: <span></span>{LOWDAY}</Text>
+            <Text>Variation last 24 hours: <span></span>{CHANGEPCT24HOUR}</Text>
+            <Text>Last Update: <span></span>{LASTUPDATE}</Text>
+        </div>
     </Container>
   )
 }
