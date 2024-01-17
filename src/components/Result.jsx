3bdd0f790680afc9e67b0ab5643f5fdc88983cf2
fernandogbz <fgjv06@gmail.com>
2024-01-17 17:27:53 -0300

    PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, and LASTUPDATE values to DOM in p's

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,7 +4,11 @@ const Result = ({result}) => {
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = result
   return (
     <div>
-        <h1>From Result</h1>
+        <p>Price is: <span>{PRICE}</span></p>
+        <p>Highest price of the day: <span></span>{HIGHDAY}</p>
+        <p>Lowest price of the day: <span></span>{LOWDAY}</p>
+        <p>Variation last 24 hours: <span></span>{CHANGEPCT24HOUR}</p>
+        <p>Last Update: <span></span>{LASTUPDATE}</p>
     </div>
   )
 }
