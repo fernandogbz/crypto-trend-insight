a88947e5cd3219b61235504523dd0a907e750bcf
fernandogbz <fgjv06@gmail.com>
2024-01-17 17:35:27 -0300

    Container, Text and Price font-family, font-size, color changed

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,10 +1,23 @@
 import styled from '@emotion/styled'
 
-const Container = styled.div``
+const Container = styled.div`
+    color: #fff;
+    font-family: 'Lato', sans-serif;
+`
 
-const Text = styled.p``
+const Text = styled.p`
+    font-size: 18px;
+    span {
+        font-weight: 700;
+    }
+`
 
-const Price = styled.div``
+const Price = styled.div`
+    font-size: 30px;
+    span {
+        font-weight: 700;
+    }
+`
 
 const Result = ({result}) => {
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = result
